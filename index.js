36def75f98bd6edc7d9f42f66a527870f27d18b5
Devang <devanggandhi2222@gmail.com>
2021-12-31 19:04:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+function myFunction() {
+	var dots = document.getElementById("dots");
+	var moreText = document.getElementById("more");
+	var btnText = document.getElementById("myBtn");
+  
+	if (dots.style.display === "none") {
+	  dots.style.display = "inline";
+	  btnText.innerHTML = "Read more"; 
+	  moreText.style.display = "none";
+	} else {
+	  dots.style.display = "none";
+	  btnText.innerHTML = "Read less"; 
+	  moreText.style.display = "inline";
+	}
+  }
+  function myFunction2() {
+	var dots = document.getElementById("dots2");
+	var moreText = document.getElementById("more2");
+	var btnText = document.getElementById("myBtn2");
+  
+	if (dots.style.display === "none") {
+	  dots.style.display = "inline";
+	  btnText.innerHTML = "Read more"; 
+	  moreText.style.display = "none";
+	} else {
+	  dots.style.display = "none";
+	  btnText.innerHTML = "Read less"; 
+	  moreText.style.display = "inline";
+	}
+  }
+  function myFunction3() {
+	var dots = document.getElementById("mn1");
+	var moreText = document.getElementById("mn2");
+	var btnText = document.getElementById("mn3");
+  
+	if (dots.style.display === "none") {
+	  dots.style.display = "inline";
+	  btnText.innerHTML = "Read more"; 
+	  moreText.style.display = "none";
+	} else {
+	  dots.style.display = "none";
+	  btnText.innerHTML = "Read less"; 
+	  moreText.style.display = "inline";
+	}
+  }
